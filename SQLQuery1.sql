9bd4f595ebe1024a6ebd5a08c625ad5ecee4aff5
penuGiT1 <penu2023@gmail.com>
2023-04-01 17:32:16 -0400

    my first file for GIT

---
+++ SQLQuery1.sql
@@ -0,0 +1,11 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [CustomerID]
+      ,[CustomerName]
+      ,[Phone]
+      ,[Address]
+      ,[City]
+      ,[State]
+      ,[Zip]
+      ,[Country]
+      ,[Notes]
+  FROM [KCC].[dbo].[Customers]
